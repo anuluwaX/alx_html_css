dd92969cda5e24fdf16b4cc70d77c23aeab35e2b
Anuoluwa <anuoluwapop3@gmail.com>
2025-06-23 01:32:48 +0200

    readme file

--- headphones/8-script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon = document.querySelector('.menu-icon');
-  const navMobile = document.querySelector('.nav-list-mobile');
-
-  menuIcon.addEventListener('click', function () {
-    navMobile.classList.toggle('show');
-  });
-});
